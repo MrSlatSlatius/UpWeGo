2ddda9f9e4341509cfcd968ac8a5f797eda86e39
PmaiWoW <PmaiWoW@Gmail.com>
2019-12-06 14:50:20 +0000

    Add and modify various assets

--- Assets/Scritps/Item.cs
+++ Assets/Scritps/Item.cs
@@ -6,10 +6,11 @@ public class Item : MonoBehaviour
 {
     public enum Tag : byte
     {
-        Potion = 0,
+        SubmarineMine = 0,
+        ForbiddenFruit = 1
     }
 
-    [SerializeField] private Tag tag = Tag.Potion;
+    [SerializeField] private Tag tag = Tag.SubmarineMine;
 
     public Tag ItemTag => tag;
 }
