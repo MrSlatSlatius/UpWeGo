751f47b3d9057e29429b40bdae89d58c03ff8338
PmaiWoW <PmaiWoW@Gmail.com>
2019-12-06 10:51:31 +0000

    Organize Assets into folders

--- Assets/Scritps/DestroyPlatform.cs
+++ Assets/Scritps/DestroyPlatform.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DestoyPlatform : MonoBehaviour
+public class DestroyPlatform : MonoBehaviour
 {
     private void Update()
     {
